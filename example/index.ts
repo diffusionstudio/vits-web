f73b3eee99f77d30e25cd83f4238c17ee624b7aa
Sam Willis <sam.willis@gmail.com>
2024-07-10 19:41:21 +0200

    Reuse session for 60% speedup

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,13 @@
 import * as tts from '../src';
 import Worker from './worker.ts?worker';
+import Worker2 from './worker2.ts?worker';
 
 // required for e2e
 Object.assign(window, { tts });
 
 document.querySelector('#app')!.innerHTML = `
 <button id="btn" type="button">Predict</button>
+<button id="btn2" type="button">Predict with Session Reuse</button>
 `
 
 document.getElementById('btn')?.addEventListener('click', async () => {
@@ -25,4 +27,22 @@ document.getElementById('btn')?.addEventListener('click', async () => {
     audio.play();
     worker.terminate();
   });
+});
+
+const mainWorker = new Worker2();
+
+document.getElementById('btn2')?.addEventListener('click', async () => {
+  mainWorker.postMessage({
+    type: 'init',
+    text: "As the waves crashed against the shore, they carried tales of distant lands and adventures untold.",
+    voiceId: 'en_US-hfc_female-medium',
+  });
+
+  mainWorker.addEventListener('message', (event: MessageEvent<{ type: 'result', audio: Blob }>) => {
+    if (event.data.type != 'result') return;
+
+    const audio = new Audio();
+    audio.src = URL.createObjectURL(event.data.audio);
+    audio.play();
+  }, { once: true });
 });
--- example/worker2.ts
+++ example/worker2.ts
@@ -1,13 +1,16 @@
 import * as tts from '../src/index';
 
+const start = performance.now();
+const TtsSession = await tts.TtsSession.create({
+  voiceId: 'en_US-hfc_female-medium',
+});
+console.log('Time taken to init session:', performance.now() - start);
+
 async function main(event: MessageEvent<tts.InferenceConfg & { type: 'init' }>) {
   if (event.data?.type != 'init') return;
 
   const start = performance.now();
-  const blob = await tts.predict({
-    text: event.data.text,
-    voiceId: event.data.voiceId,
-  });
+  const blob = await TtsSession.predict(event.data.text);
   console.log('Time taken:', performance.now() - start);
 
   self.postMessage({ type: 'result', audio: blob })
